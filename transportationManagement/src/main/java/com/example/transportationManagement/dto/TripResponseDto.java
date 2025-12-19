1168cb6085b29c24c8d078207d18aae9dabff7e0
ajay-ackerman <ajayawchar8007@gmail.com>
2025-12-19 11:25:55 +0530

    Minor changes

--- transportationManagement/src/main/java/com/example/transportationManagement/dto/TripResponseDto.java
+++ transportationManagement/src/main/java/com/example/transportationManagement/dto/TripResponseDto.java
@@ -33,6 +33,8 @@ public class TripResponseDto {
     private String scheduledEnd;
 
     private TripStatus status;
+    private String Source;
+    private String destination;
 
     private int bookedSeats; // tickets.size()
 }
